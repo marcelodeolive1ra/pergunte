486e59b18303d6bb71f14462830d836c121c6302
Danilo Guarnieri Cardoso <cardoso.nilo@gmail.com>
2017-01-07 18:25:43 -0200

    Adding logs and Internet permission

--- app/src/main/java/mds/ufscar/pergunte/MainActivity.java
+++ app/src/main/java/mds/ufscar/pergunte/MainActivity.java
@@ -3,18 +3,8 @@ package mds.ufscar.pergunte;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -51,7 +41,8 @@ public class MainActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if(firebaseAuth.getCurrentUser() != null){
                     startActivity(new Intent(MainActivity.this,AccountActivity.class));
-                }
+                } else
+                    Log.w("firebase", "firebaseAuth.getCurrentUser is null");
             }
         };
         mGoogleBtn = (SignInButton) findViewById(R.id.googleBtn);
@@ -106,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
                 firebaseAuthWithGoogle(account);
             } else {
                 // Google Sign In failed, update UI appropriately
-                // ...
+                Log.w("GoogleSignIn", "Google Sign In failed");
             }
         }
     }
