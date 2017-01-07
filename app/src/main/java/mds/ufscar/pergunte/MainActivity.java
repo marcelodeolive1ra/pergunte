0b3c162f2ae13c212c158df53835e58dc6e24121
Danilo Guarnieri Cardoso <cardoso.nilo@gmail.com>
2017-01-07 18:22:41 -0200

    Revert "Adding logs and Internet permission"
    This reverts commit 0183e562291ebccfa38250679a17a2af67be623c.

--- app/src/main/java/mds/ufscar/pergunte/MainActivity.java
+++ app/src/main/java/mds/ufscar/pergunte/MainActivity.java
@@ -3,8 +3,18 @@ package mds.ufscar.pergunte;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -40,9 +50,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if(firebaseAuth.getCurrentUser() != null){
-                    startActivity(new Intent(MainActivity.this, AccountActivity.class));
-                } else
-                    Log.w("firebase", "getCurrentUser = null");
+                    startActivity(new Intent(MainActivity.this,AccountActivity.class));
+                }
             }
         };
         mGoogleBtn = (SignInButton) findViewById(R.id.googleBtn);
@@ -97,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
                 firebaseAuthWithGoogle(account);
             } else {
                 // Google Sign In failed, update UI appropriately
-                Log.w("GoogleSignIn", "Google Sign In failed");
+                // ...
             }
         }
     }
