4143ae1080c3cbe2be99d9fd6cff6f438c2a6107
ThiagoYonamine <thyonamine1@gmail.com>
2017-01-14 01:54:25 -0200

    Adicionado leitor de QR code no botao +

--- app/src/main/java/mds/ufscar/pergunte/MainScreen.java
+++ app/src/main/java/mds/ufscar/pergunte/MainScreen.java
@@ -2,6 +2,7 @@ package mds.ufscar.pergunte;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,16 +11,21 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.zxing.Result;
 
-public class MainScreen extends AppCompatActivity {
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainScreen extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -39,6 +45,7 @@ public class MainScreen extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthListener;
     private String mPerfil;
     private boolean mProfessor;
+    private ZXingScannerView mScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,14 +74,15 @@ public class MainScreen extends AppCompatActivity {
         tabLayout.setupWithViewPager(mViewPager);
         mViewPager.setCurrentItem(1);   // tab Materias is the default tab
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                  //      .setAction("Action", null).show();
+
             }
-        });
+        });*/
 
         // authentication code
         mAuth = FirebaseAuth.getInstance();
@@ -88,6 +96,17 @@ public class MainScreen extends AppCompatActivity {
         };
     }
 
+    public void onClick(View view) {
+        mScanner = new ZXingScannerView(this);
+        setContentView(mScanner);
+        mScanner.setResultHandler(this);
+        mScanner.startCamera();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mScanner.stopCamera();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -108,6 +127,27 @@ public class MainScreen extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Log.v("handleResult", result.getText());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan");
+        builder.setMessage(result.getText());
+        AlertDialog alert = builder.create();
+        alert.show();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                mScanner.stopCamera();
+                finish();
+                startActivity(getIntent());
+            }
+        }, 2000);
+
+
+
     }
 
     /**
