d705edd27575a2e6bf1c266d3f56385c1905536e
Danilo Guarnieri Cardoso <cardoso.nilo@gmail.com>
2017-01-14 23:17:40 -0200

    Adicionando cadastro de materia (interface pronta)

--- app/src/main/java/mds/ufscar/pergunte/MainScreen.java
+++ app/src/main/java/mds/ufscar/pergunte/MainScreen.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -71,8 +70,10 @@ public class MainScreen extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (mProfessor) {
+                    Intent cadastroMateria = new Intent(MainScreen.this, CadastroMateria.class);
+                    startActivity(cadastroMateria);
+                }
             }
         });
 
