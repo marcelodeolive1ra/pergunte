bf878558e4d01a0c8e7b14252606f36ae760f2e4
marcelodeolive1ra <marcelodeoliveira@outlook.com>
2017-01-17 10:32:46 -0200

    Implementa construção de objetos Materia via JSON como um método

--- app/src/main/java/mds/ufscar/pergunte/model/Materia.java
+++ app/src/main/java/mds/ufscar/pergunte/model/Materia.java
@@ -1,5 +1,8 @@
 package mds.ufscar.pergunte.model;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class Materia {
@@ -16,6 +19,8 @@ public class Materia {
 
     public Materia(int codigo, String turma, int ano, int semestre, String nomeDisciplina,
                    Professor professor, String codigoInscricao) {
+        this.perguntas = new ArrayList<>();
+        this.alunos = new ArrayList<>();
         this.setCodigo(codigo);
         this.setTurma(turma);
         this.setAno(ano);
@@ -28,9 +33,40 @@ public class Materia {
         imageUrl = "https://pt.wikipedia.org/wiki/Turma_da_M%C3%B4nica#/media/File:Turma_da_M%C3%B4nica.jpg";
     }
 
+    public boolean construirObjetoComJSON(JSONObject resultado_requisicao) {
+        this.perguntas = new ArrayList<>();
+        this.alunos = new ArrayList<>();
+        JSONObject professor_json = null;
+        try {
+            professor_json = resultado_requisicao.getJSONObject("professor");
+            this.setAno(resultado_requisicao.getInt("codigo"));
+            this.setTurma(resultado_requisicao.getString("turma"));
+            this.setAno(resultado_requisicao.getInt("ano"));
+            this.setSemestre(resultado_requisicao.getInt("semestre"));
+            this.setNomeDisciplina(resultado_requisicao.getString("nome_materia"));
+            this.setProfessor(new Professor(
+                    professor_json.getString("nome"),
+                    professor_json.getString("sobrenome"),
+                    professor_json.getString("email"),
+                    professor_json.getString("universidade")));
+            this.setCodigoInscricao(resultado_requisicao.getString("codigo_inscricao"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
     public Materia(int codigo, String turma, int ano, int semestre, String nomeDisciplina,
                    String codigoInscricao) {
         this(codigo, turma, ano, semestre, nomeDisciplina, null, codigoInscricao);
+        this.perguntas = new ArrayList<>();
+        this.alunos = new ArrayList<>();
+    }
+
+    public Materia() {
+        this.perguntas = new ArrayList<>();
+        this.alunos = new ArrayList<>();
     }
 
     public void setCodigo(int codigo) {
