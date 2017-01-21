877f73421d8f2641e945009fb40cb5b6480b7474
marcelodeolive1ra <marcelodeoliveira@outlook.com>
2017-01-21 00:23:06 -0200

    Fixes #22 - corrige travamento do app ao abrí-lo sem uma conexão com a Internet

--- app/src/main/java/mds/ufscar/pergunte/RequisicaoAssincrona.java
+++ app/src/main/java/mds/ufscar/pergunte/RequisicaoAssincrona.java
@@ -50,73 +50,77 @@ public class RequisicaoAssincrona extends AsyncTask<String, Void, JSONObject> {
     }
 
     protected JSONObject doInBackground(String... params) {
-        Map<String, String> data = new HashMap<>();
+        Map<String, String> parametros = new HashMap<>();
         JSONObject retorno_requisicao = null;
         HttpRequest request = null;
 
-        switch (params[0]) {
-            case BUSCAR_ALUNO:
-                data.put(Parametros.EMAIL_USUARIO, params[1]);
-                data.put(Parametros.TIPO_USUARIO, Parametros.PERFIL_ALUNO);
-                request = HttpRequest.post("http://mds.secompufscar.com.br/buscaraluno/").form(data);
-                break;
-
-            case BUSCAR_MATERIAS:
-                data.put(Parametros.EMAIL_USUARIO, params[1]);
-                data.put(Parametros.TIPO_USUARIO, params[2]);
-                data.put(Parametros.STATUS_MATERIA, params[3]);
-                request = HttpRequest.post("http://mds.secompufscar.com.br/buscarmaterias/").form(data);
-                break;
-
-            case BUSCAR_MATERIA_POR_QR_CODE:
-                data.put(Parametros.EMAIL_USUARIO, params[1]);
-                data.put(Parametros.CODIGO_MATERIA, params[2]);
-                request = HttpRequest.post("http://mds.secompufscar.com.br/buscarmateriaporqr/").form(data);
-                break;
-
-            case CADASTRAR_NOVA_MATERIA:
-                Materia materia = (Materia)objetoGenerico;
-                // Não é necessário passar o código, pois este será gerado automaticamente no banco
-                data.put(Parametros.EMAIL_USUARIO, params[1]);
-                data.put(Parametros.TURMA_MATERIA, materia.getTurma());
-                data.put(Parametros.ANO_MATERIA, Integer.toString(materia.getAno()));
-                data.put(Parametros.SEMESTRE_MATERIA, Integer.toString(materia.getSemestre()));
-                data.put(Parametros.NOME_MATERIA, materia.getNomeDisciplina());
-                data.put(Parametros.CODIGO_INSCRICAO_MATERIA, materia.getCodigoInscricao());
-                request = HttpRequest.post("http://mds.secompufscar.com.br/cadastrarmateria/").form(data);
-                break;
-
-            case INSCREVER_ALUNO_EM_MATERIA:
-                data.put(Parametros.EMAIL_USUARIO, params[1]);
-                data.put(Parametros.CODIGO_MATERIA, params[2]);
-                request = HttpRequest.post("http://mds.secompufscar.com.br/inscreveralunoemmateria/").form(data);
-                break;
-
-            case CANCELAR_INSCRICAO_EM_MATERIA:
-                data.put(Parametros.EMAIL_USUARIO, params[1]);
-                data.put(Parametros.CODIGO_MATERIA, params[2]);
-                request = HttpRequest.post("http://mds.secompufscar.com.br/cancelarinscricaoemmateria/").form(data);
-                break;
-
-            case BUSCAR_PERFIL_DO_USUARIO:
-                data.put(Parametros.EMAIL_USUARIO, params[1]);
-                request = HttpRequest.post("http://mds.secompufscar.com.br/buscarperfilusuario/").form(data);
-                break;
-
-            case DESATIVAR_MATERIA:
-                data.put(Parametros.EMAIL_USUARIO, params[1]);
-                data.put(Parametros.CODIGO_MATERIA, params[2]);
-                request = HttpRequest.post("http://mds.secompufscar.com.br/desativarmateria/").form(data);
-                break;
-
-            default:
-                break;
-        }
-
         try {
-            retorno_requisicao = (request != null) ? new JSONObject(request.body()) : null;
-        } catch (JSONException e) {
-            e.printStackTrace();
+            switch (params[0]) {
+                case BUSCAR_ALUNO:
+                    parametros.put(Parametros.EMAIL_USUARIO, params[1]);
+                    parametros.put(Parametros.TIPO_USUARIO, Parametros.PERFIL_ALUNO);
+                    request = HttpRequest.post("http://mds.secompufscar.com.br/buscaraluno/").form(parametros);
+                    break;
+
+                case BUSCAR_MATERIAS:
+                    parametros.put(Parametros.EMAIL_USUARIO, params[1]);
+                    parametros.put(Parametros.TIPO_USUARIO, params[2]);
+                    parametros.put(Parametros.STATUS_MATERIA, params[3]);
+                    request = HttpRequest.post("http://mds.secompufscar.com.br/buscarmaterias/").form(parametros);
+                    break;
+
+                case BUSCAR_MATERIA_POR_QR_CODE:
+                    parametros.put(Parametros.EMAIL_USUARIO, params[1]);
+                    parametros.put(Parametros.CODIGO_MATERIA, params[2]);
+                    request = HttpRequest.post("http://mds.secompufscar.com.br/buscarmateriaporqr/").form(parametros);
+                    break;
+
+                case CADASTRAR_NOVA_MATERIA:
+                    Materia materia = (Materia) objetoGenerico;
+                    // Não é necessário passar o código, pois este será gerado automaticamente no banco
+                    parametros.put(Parametros.EMAIL_USUARIO, params[1]);
+                    parametros.put(Parametros.TURMA_MATERIA, materia.getTurma());
+                    parametros.put(Parametros.ANO_MATERIA, Integer.toString(materia.getAno()));
+                    parametros.put(Parametros.SEMESTRE_MATERIA, Integer.toString(materia.getSemestre()));
+                    parametros.put(Parametros.NOME_MATERIA, materia.getNomeDisciplina());
+                    parametros.put(Parametros.CODIGO_INSCRICAO_MATERIA, materia.getCodigoInscricao());
+                    request = HttpRequest.post("http://mds.secompufscar.com.br/cadastrarmateria/").form(parametros);
+                    break;
+
+                case INSCREVER_ALUNO_EM_MATERIA:
+                    parametros.put(Parametros.EMAIL_USUARIO, params[1]);
+                    parametros.put(Parametros.CODIGO_MATERIA, params[2]);
+                    request = HttpRequest.post("http://mds.secompufscar.com.br/inscreveralunoemmateria/").form(parametros);
+                    break;
+
+                case CANCELAR_INSCRICAO_EM_MATERIA:
+                    parametros.put(Parametros.EMAIL_USUARIO, params[1]);
+                    parametros.put(Parametros.CODIGO_MATERIA, params[2]);
+                    request = HttpRequest.post("http://mds.secompufscar.com.br/cancelarinscricaoemmateria/").form(parametros);
+                    break;
+
+                case BUSCAR_PERFIL_DO_USUARIO:
+                    parametros.put(Parametros.EMAIL_USUARIO, params[1]);
+                    request = HttpRequest.post("http://mds.secompufscar.com.br/buscarperfilusuario/").form(parametros);
+                    break;
+
+                case DESATIVAR_MATERIA:
+                    parametros.put(Parametros.EMAIL_USUARIO, params[1]);
+                    parametros.put(Parametros.CODIGO_MATERIA, params[2]);
+                    request = HttpRequest.post("http://mds.secompufscar.com.br/desativarmateria/").form(parametros);
+                    break;
+
+                default:
+                    break;
+            }
+
+            try {
+                retorno_requisicao = (request != null) ? new JSONObject(request.body()) : null;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } catch (HttpRequest.HttpRequestException e) {
+            return null;
         }
 
         return retorno_requisicao;
--- app/src/main/java/mds/ufscar/pergunte/Tab2_Materias.java
+++ app/src/main/java/mds/ufscar/pergunte/Tab2_Materias.java
@@ -59,24 +59,37 @@ public class Tab2_Materias extends Fragment {
                     emailUsuarioAtual, perfil_usuario,
                     RequisicaoAssincrona.Parametros.STATUS_MATERIA_ATIVA).get();
 
-            if (resultado_requisicao.getString("status").equals("ok")) {
-                JSONArray materias_json = resultado_requisicao.getJSONArray("materias");
+            if (resultado_requisicao != null) {
+                if (resultado_requisicao.getString("status").equals("ok")) {
+                    JSONArray materias_json = resultado_requisicao.getJSONArray("materias");
 
-                ArrayList<Materia> materias = new ArrayList<>();
-                for (int i = 0; i < materias_json.length(); i++) {
-                    Materia materia = new Materia();
-                    if (materia.construirObjetoComJSON(materias_json.getJSONObject(i))) {
-                        materias.add(materia);
-                    } else {
-                        Toast.makeText(Tab2_Materias.this.getActivity(),
-                                "Erro ao carregar matéria.", Toast.LENGTH_LONG).show();
+                    ArrayList<Materia> materias = new ArrayList<>();
+                    for (int i = 0; i < materias_json.length(); i++) {
+                        Materia materia = new Materia();
+                        if (materia.construirObjetoComJSON(materias_json.getJSONObject(i))) {
+                            materias.add(materia);
+                        } else {
+                            Toast.makeText(Tab2_Materias.this.getActivity(),
+                                    "Erro ao carregar matéria.", Toast.LENGTH_LONG).show();
+                        }
                     }
+                    mMateriaItems = addSections(materias);
+                } else {
+                    Log.w("REQUISICAO", resultado_requisicao.toString());
+                    Toast.makeText(Tab2_Materias.this.getActivity(),
+                            resultado_requisicao.getString("descricao"), Toast.LENGTH_LONG).show();
                 }
-                mMateriaItems = addSections(materias);
             } else {
-                Log.w("REQUISICAO", resultado_requisicao.toString());
-                Toast.makeText(Tab2_Materias.this.getActivity(),
-                        resultado_requisicao.getString("descricao"), Toast.LENGTH_LONG).show();
+                AlertDialog.Builder adb = new AlertDialog.Builder(Tab2_Materias.this.getActivity());
+                adb.setTitle("Erro");
+                adb.setMessage("Não foi possível conectar à Internet.\n\nVerifique sua conexão e tente novamente.");
+                adb.setPositiveButton("Tentar novamente", new AlertDialog.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                        System.exit(0);
+                    }
+                });
+                adb.show();
             }
 
         } catch (InterruptedException | ExecutionException | JSONException e) {
