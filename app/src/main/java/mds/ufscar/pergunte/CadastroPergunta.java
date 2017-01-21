d0f9212421155cf0d0ba977548e043f0b28d4814
Danilo Guarnieri Cardoso <cardoso.nilo@gmail.com>
2017-01-21 12:42:44 -0200

    Cadastro Pergunta finalizado e validado!

--- app/src/main/java/mds/ufscar/pergunte/CadastroPergunta.java
+++ app/src/main/java/mds/ufscar/pergunte/CadastroPergunta.java
@@ -7,8 +7,9 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
@@ -28,6 +29,8 @@ import java.util.Locale;
 import mds.ufscar.pergunte.model.Alternativa;
 import mds.ufscar.pergunte.model.Pergunta;
 
+import static mds.ufscar.pergunte.R.id.radiogroup;
+
 /**
  * Created by Danilo on 15/01/2017.
  */
@@ -41,16 +44,12 @@ public class CadastroPergunta extends AppCompatActivity {
     private Button mAddAlternative;
     private Button mCadastrarPergunta;
     private TextView mSelectedDate;
-    private int mYear;
-    private int mMonth;
-    private int mDay;
     private DatePickerDialog mDatePickerDialog;
-    private SimpleDateFormat dateFormatter;
-    private String mTextAlternative;
+    private SimpleDateFormat mDateFormat;
+    private RadioGroup mRadioGroup;
     private final int maxAlternativas = 5;
     private final String mAlternativeLetters[] = {"A", "B", "C", "D", "E"};
     private int index;
-    private int correta;
     private ArrayList<Alternativa> mAlternativas;
     private int mCodigoMateria;
 
@@ -58,7 +57,7 @@ public class CadastroPergunta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_pergunta);
-        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.CANADA);
+        mDateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.CANADA);
         final Context context = this;
         index = 0;  // para letra da alternativa
         mAlternativas = new ArrayList<>();
@@ -72,16 +71,18 @@ public class CadastroPergunta extends AppCompatActivity {
         mSelectedDate = (TextView)findViewById(R.id.input_selectedDate);
         mEditAlternative = (Button)findViewById(R.id.btn_editAlternative);
         mAddAlternative = (Button)findViewById(R.id.btn_newAlternative);
+        mRadioGroup = (RadioGroup)findViewById(radiogroup);
         mCadastrarPergunta = (Button)findViewById(R.id.btn_cadastrar);
 
         setCurrentDateOnView();
+        mRadioGroup.setOrientation(LinearLayout.VERTICAL);
 
         Calendar newCalendar = Calendar.getInstance();
         mDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                 Calendar newDate = Calendar.getInstance();
                 newDate.set(year, monthOfYear, dayOfMonth);
-                mSelectedDate.setText(dateFormatter.format(newDate.getTime()));
+                mSelectedDate.setText(mDateFormat.format(newDate.getTime()));
             }
         },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
 
@@ -90,6 +91,7 @@ public class CadastroPergunta extends AppCompatActivity {
         mDateSelector.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mDatePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
                 mDatePickerDialog.show();
             }
         });
@@ -109,19 +111,23 @@ public class CadastroPergunta extends AppCompatActivity {
                     final EditText alternativa = (EditText) dialog.findViewById(R.id.input_alternative);
                     final CheckBox correta = (CheckBox) dialog.findViewById(R.id.checkboxCorreta);
 
-                    Button cancelButton = (Button) dialog.findViewById(R.id.btn_cancel);
-                    Button okButton = (Button) dialog.findViewById(R.id.btn_ok);
+                    final Button cancelButton = (Button) dialog.findViewById(R.id.btn_cancel);
+                    final Button okButton = (Button) dialog.findViewById(R.id.btn_ok);
+
+                    okButton.setEnabled(false);
+                    addTextChangedListener(alternativa, okButton);
+
                     // if button ok is clicked, add alternative and close the custom dialog
                     okButton.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            String textoAlternativa =
-                                    mAlternativeLetters[index] + ") " + alternativa.getText().toString();
-                            addRadioButton(textoAlternativa, correta.isChecked());
+                            String textoAlternativa = alternativa.getText().toString();
                             // criando objecto alternativa
                             Alternativa alternativa = new Alternativa(mAlternativeLetters[index],
                                     textoAlternativa, correta.isChecked());
                             mAlternativas.add(alternativa);
+                            index++;
+                            setRadioButtons(mAlternativas);
                             dialog.dismiss();
                         }
                     });
@@ -150,49 +156,149 @@ public class CadastroPergunta extends AppCompatActivity {
         mCadastrarPergunta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Date date = new Date();
-                try {
-                    date = dateFormatter.parse(mSelectedDate.getText().toString());
-                } catch (ParseException ex) {
-                    ex.printStackTrace();
+                String warning = null;
+                if (mAlternativas.size() == 0 ) {
+                    warning = "Deve existir pelo menos uma alternativa";
                 }
-                // criando objeto pergunta
-                Pergunta pergunta = new Pergunta(
-                        mInputTitle.getText().toString(),
-                        mInputQuestion.getText().toString(),
-                        mAlternativas,
-                        date
-                );
+                if (mInputQuestion.getText().toString().trim().length() == 0) {
+                    warning = "Pergunta deve ser preenchida";
+                }
+                if (mInputTitle.getText().toString().trim().length() == 0) {
+                    warning = "Título deve ser preenchido";
+                }
+
+                if (warning == null) {  // se validou os dados (sem warnings)
+                    Date date = new Date();
+                    try {
+                        date = mDateFormat.parse(mSelectedDate.getText().toString());
+                    } catch (ParseException ex) {
+                        ex.printStackTrace();
+                    }
+                    // criando objeto pergunta
+                    Pergunta pergunta = new Pergunta(
+                            mInputTitle.getText().toString(),
+                            mInputQuestion.getText().toString(),
+                            mAlternativas,
+                            date
+                    );
 
 
+                    // TODO: Marcelo, persista no BD. Grato.
 
-                // TODO: Marcelo, persista no BD. Grato.
+                    finish();
+                } else {
+                    new AlertDialog.Builder(context)
+                            .setTitle("Atenção")
+                            .setMessage(warning)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {}
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }
+            }
+        });
 
-                finish();
+        mEditAlternative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int radioButtonID = mRadioGroup.getCheckedRadioButtonId();
+                View radioButton = mRadioGroup.findViewById(radioButtonID);
+                final int pos = mRadioGroup.indexOfChild(radioButton);
+
+                if (pos > -1) {
+                    // custom dialog
+                    final Dialog dialog = new Dialog(context);
+                    dialog.setContentView(R.layout.alternativa);
+
+                    // set the custom dialog components - text, editText and button
+                    final TextView letra = (TextView) dialog.findViewById(R.id.label_alternativa);
+                    String title = "Alternativa - " + mAlternativeLetters[pos];
+                    letra.setText(title);
+                    final EditText alternativa = (EditText) dialog.findViewById(R.id.input_alternative);
+                    alternativa.setText(mAlternativas.get(pos).getTextoAlternativa());
+                    final CheckBox correta = (CheckBox) dialog.findViewById(R.id.checkboxCorreta);
+                    correta.setChecked(mAlternativas.get(pos).isCorreta());
+
+                    final Button cancelButton = (Button) dialog.findViewById(R.id.btn_cancel);
+                    String excluir = "Excluir";
+                    cancelButton.setText(excluir);
+                    final Button okButton = (Button) dialog.findViewById(R.id.btn_ok);
+                    String salvar = "Salvar";
+                    okButton.setText(salvar);
+
+                    addTextChangedListener(alternativa, okButton);
+
+                    // if button Salvar is clicked, edit alternative and close the custom dialog
+                    okButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String textoAlternativa = alternativa.getText().toString();
+                            // editando objecto alternativa
+                            mAlternativas.get(pos).setTextoAlternativa(textoAlternativa);
+                            mAlternativas.get(pos).setCorreta(correta.isChecked());
+                            setRadioButtons(mAlternativas);
+                            dialog.dismiss();
+                        }
+                    });
+                    // excluir alternativa
+                    cancelButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            mAlternativas.remove(pos);
+                            // reset letters
+                            index = 0;
+                            for (Alternativa alternativa : mAlternativas) {
+                                alternativa.setLetra(mAlternativeLetters[index]);
+                                index++;
+                            }
+                            setRadioButtons(mAlternativas);
+                            dialog.dismiss();
+                        }
+                    });
+
+                    dialog.show();
+                }
             }
         });
     }
 
     private void setCurrentDateOnView(){
-        final Calendar c = Calendar.getInstance();
-        mYear = c.get(Calendar.YEAR);
-        mMonth = c.get(Calendar.MONTH);
-        mDay = c.get(Calendar.DAY_OF_MONTH);
-
+        final Date actualDate = Calendar.getInstance().getTime();
         // set current date into textview
-        mSelectedDate.setText(new StringBuilder()
-                // Month is 0 based, just add 1
-                .append(mDay).append("-").append(mMonth + 1).append("-")
-                .append(mYear));
+        mSelectedDate.setText(mDateFormat.format(actualDate));
     }
 
-    public void addRadioButton(String text, boolean correct) {
-        RadioGroup radioGroup = new RadioGroup(this);
-        radioGroup.setOrientation(LinearLayout.VERTICAL);
-        RadioButton rdbtn = new RadioButton(this);
-        rdbtn.setText(text);
-        radioGroup.addView(rdbtn);
-        ((ViewGroup) findViewById(R.id.radiogroup)).addView(radioGroup);
-        index++;
+    public void setRadioButtons(ArrayList<Alternativa> alternativas) {
+        mRadioGroup.removeAllViews();   // clearing added buttons if any
+        for (Alternativa alternativa : alternativas) {
+            RadioButton rdbtn = new RadioButton(this);
+            String textoComLetra = alternativa.getLetra() + ") " + alternativa.getTextoAlternativa();
+            rdbtn.setText(textoComLetra);
+            mRadioGroup.addView(rdbtn);
+        }
+    }
+
+    public void addTextChangedListener(final EditText editText, final Button btn) {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (editText.getText().toString().trim().length() > 0) {
+                    btn.setEnabled(true);
+                } else {
+                    btn.setEnabled(false);
+                }
+            }
+        });
     }
 }
