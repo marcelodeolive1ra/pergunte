5e9a2fd2d0653ee98523d6be5768d76f137c276e
marcelodeolive1ra <marcelodeoliveira@outlook.com>
2017-01-30 17:07:51 -0200

    Melhorias na splash screen

--- app/src/main/java/mds/ufscar/pergunte/SplashScreenActivity.java
+++ app/src/main/java/mds/ufscar/pergunte/SplashScreenActivity.java
@@ -2,16 +2,32 @@ package mds.ufscar.pergunte;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen_layout);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        int SPLASH_TIME_OUT = 2000;
+
+        Toolbar splashScreenToolbar = (Toolbar)findViewById(R.id.splash_scrren_toolbar);
+        splashScreenToolbar.setTitle("Pergunte");
+        splashScreenToolbar.setTitleTextColor(getResources().getColor(R.color.white));
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
