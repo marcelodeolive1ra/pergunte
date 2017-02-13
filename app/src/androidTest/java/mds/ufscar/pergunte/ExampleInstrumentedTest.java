61d9db75a16871a6f3eb727b8992a91daaeb700a
Rodolfo Barcelar <ro_barcelar@yahoo.com.br>
2017-02-13 01:13:09 -0200

    Modificações e realização de novos testes

--- app/src/androidTest/java/mds/ufscar/pergunte/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/mds/ufscar/pergunte/ExampleInstrumentedTest.java
@@ -21,17 +21,20 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void adicionaMateria1(){
-        String emailUsuarioAtual = "cardoso.nilo@gmail.com";
-        float passouTeste;
+        double resultado;
         Materia materia = new Materia();
         RequisicaoAssincrona requisicao = new RequisicaoAssincrona();
         requisicao.setObject(materia);
         try {
-            JSONObject resultado_requisicao = requisicao.execute(RequisicaoAssincrona.CADASTRAR_NOVA_MATERIA,
-                    emailUsuarioAtual).get();
-            if (resultado_requisicao.getString("status").equals("ok")){ passouTeste = 1; }
-            else{ passouTeste = 0; }
-            assertEquals(0, passouTeste, 0f);
+            JSONObject resultado_requisicao = requisicao.execute(RequisicaoAssincrona.CADASTRAR_NOVA_MATERIA, "rophos.rb@gmail.com").get();
+            if(resultado_requisicao.getString("status").equals("error")){
+                if(resultado_requisicao.getString("descricao").equals("Parâmetros inválidos.")){resultado = 1.0;}
+                else if(resultado_requisicao.getString("descricao").equals("Código de inscrição já cadastrado para outra matéria.")){resultado = 2.0;}
+                else if(resultado_requisicao.getString("descricao").equals("Professor não cadastrado.")){resultado = 3.0;}
+                else{resultado = 0;}
+            }else{resultado = -1.0;}
+
+            assertEquals(1.0,resultado,0.0f);
         }catch (InterruptedException | ExecutionException | JSONException e) {
             e.printStackTrace();
         }
@@ -39,19 +42,193 @@ public class ExampleInstrumentedTest {
 
     @Test
     public void adicionaMateria2(){
-        Professor professor = new Professor("Danilo", "Cardoso","cardoso.nilo@gmail.com", "UFSCar");
-        String emailUsuarioAtual = "cardoso.nilo@gmail.com";
-        int passouTeste;
-        Materia materia = new Materia(0, "A", 2016, 2, "MDS", professor, "01");
+        Professor professor = new Professor("Rodolfo", "Barcelar","rophos.rb@gmail.com", "UFSCar");
+        double resultado;
+        Materia materia = new Materia(0, "A", 2016, 2, "MDS", professor, "MDS01");
         RequisicaoAssincrona requisicao = new RequisicaoAssincrona();
         requisicao.setObject(materia);
         try {
             JSONObject resultado_requisicao = requisicao.execute(RequisicaoAssincrona.CADASTRAR_NOVA_MATERIA,
-                    emailUsuarioAtual).get();
-            if (resultado_requisicao.getString("status").equals("ok")){ passouTeste = 1; }
-            else{ passouTeste = 0; }
-            assertEquals(1, passouTeste, 0f);
+                    "rophos.rb@gmail.com").get();
+            if (resultado_requisicao.getString("status").equals("ok")){resultado = 1.0;}
+            else{
+                if(resultado_requisicao.getString("descricao").equals("Parâmetros inválidos.")){resultado = 2.0;}
+                else if(resultado_requisicao.getString("descricao").equals("Código de inscrição já cadastrado para outra matéria.")){resultado = 3.0;}
+                else if(resultado_requisicao.getString("descricao").equals("Professor não cadastrado.")){resultado = 4.0;}
+                else{resultado = 0;}
+            }
+
+            assertEquals(1.0, resultado, 0.0f);
         }catch (InterruptedException | ExecutionException| JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void adicionaMateria3(){
+        Professor professor = new Professor("Rodolfo", "Barcelar","rophos.rb@gmail.com", "UFSCar");
+        double resultado;
+        Materia materia1 = new Materia(0, "A", 2016, 2, "MDS", professor, "MDS02");
+        RequisicaoAssincrona requisicao1 = new RequisicaoAssincrona();
+        requisicao1.setObject(materia1);
+        try {
+            JSONObject resultado_requisicao1 = requisicao1.execute(RequisicaoAssincrona.CADASTRAR_NOVA_MATERIA, "rophos.rb@gmail.com").get();
+        }catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Materia materia2 = new Materia(1, "B", 2016, 2, "CAP", professor, "MDS02");
+        RequisicaoAssincrona requisicao2 = new RequisicaoAssincrona();
+        requisicao1.setObject(materia2);
+        try {
+            JSONObject resultado_requisicao2 = requisicao2.execute(RequisicaoAssincrona.CADASTRAR_NOVA_MATERIA, "rophos.rb@gmail.com").get();
+
+            if(resultado_requisicao2.getString("status").equals("error")){
+                if(resultado_requisicao2.getString("descricao").equals("Parâmetros inválidos.")){resultado = 1.0;}
+                else if(resultado_requisicao2.getString("descricao").equals("Código de inscrição já cadastrado para outra matéria.")){resultado = 2.0;}
+                else if(resultado_requisicao2.getString("descricao").equals("Professor não cadastrado.")){resultado = 3.0;}
+                else{resultado = 0;}
+            }else{resultado = -1.0;}
+
+            assertEquals(1.0,resultado,0.0f);
+        }catch (InterruptedException | ExecutionException| JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void buscarAluno1(){
+        double resultado;
+        RequisicaoAssincrona requisicao = new RequisicaoAssincrona();
+        try {
+            JSONObject resultado_requisicao = requisicao.execute(RequisicaoAssincrona.BUSCAR_PERFIL_DO_USUARIO, "rophos.rb@gmail.com").get();
+            if(resultado_requisicao.getString("status").equals("ok")){resultado = 1.0;}
+            else{
+                if(resultado_requisicao.getString("descricao").equals("Perfil não encontrado")){resultado = 2.0;}
+                else{resultado = 0;}
+            }
+
+            assertEquals(1.0,resultado,0.0f);
+        }catch (InterruptedException | ExecutionException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void buscarAluno2(){
+        double resultado;
+        RequisicaoAssincrona requisicao = new RequisicaoAssincrona();
+        try {
+            JSONObject resultado_requisicao = requisicao.execute(RequisicaoAssincrona.BUSCAR_PERFIL_DO_USUARIO, "abc@gmail.com").get();
+            if(resultado_requisicao.getString("status").equals("ok")){resultado = 1.0;}
+            else{
+                if(resultado_requisicao.getString("descricao").equals("Perfil não encontrado")){resultado = 2.0;}
+                else{resultado = 0;}
+            }
+
+            assertEquals(2.0,resultado,0.0f);
+        }catch (InterruptedException | ExecutionException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void inscreverAlunoEmMateria1(){
+        double resultado;
+        RequisicaoAssincrona requisicao = new RequisicaoAssincrona();
+        try {
+            JSONObject resultado_requisicao = requisicao.execute(RequisicaoAssincrona.INSCREVER_ALUNO_EM_MATERIA, "rophos.rb@gmail.com", "MDS01").get();
+            if (resultado_requisicao.getString("status").equals("ok")){resultado = 1.0;}
+            else{
+                if(resultado_requisicao.getString("descricao").equals("Não é possível cadastrá-lo nesta matéria, pois ela está inativa")){resultado = 2.0;}
+                else if(resultado_requisicao.getString("descricao").equals("Erro na requisição. Aluno ou matéria não encontrados.")){resultado = 3.0;}
+                else {resultado = 0;}
+            }
+
+            assertEquals(1.0, resultado, 0.0f);
+        }catch (InterruptedException | ExecutionException| JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* CASO MATERIA JA CADASTRADA
+    @Test
+    public void inscreverAlunoEmMateria2(){
+        double resultado;
+        RequisicaoAssincrona requisicao1 = new RequisicaoAssincrona();
+        try {
+            JSONObject resultado_requisicao1 = requisicao1.execute(RequisicaoAssincrona.INSCREVER_ALUNO_EM_MATERIA, "rophos.rb@gmail.com", "MDS01").get();
+        }catch (InterruptedException | ExecutionException| JSONException e) {
+            e.printStackTrace();
+        }
+
+        RequisicaoAssincrona requisicao2 = new RequisicaoAssincrona();
+        try {
+            JSONObject resultado_requisicao2 = requisicao2.execute(RequisicaoAssincrona.INSCREVER_ALUNO_EM_MATERIA, "rophos.rb@gmail.com", "MDS01").get();
+            if (resultado_requisicao.getString("status").equals("ok")){resultado = 1.0;}
+            else{
+                if(resultado_requisicao.getString("descricao").equals("Não é possível cadastrá-lo nesta matéria, pois ela está inativa")){resultado = 2.0;}
+                else if(resultado_requisicao.getString("descricao").equals("Erro na requisição. Aluno ou matéria não encontrados.")){resultado = 3.0;}
+                else {resultado = 0;}
+            }
+
+            assertEquals(1.0, resultado, 0.0f);
+        }catch (InterruptedException | ExecutionException| JSONException e) {
+            e.printStackTrace();
+        }
+    }*/
+
+    @Test
+    public void inscreverAlunoEmMateria3(){
+        double resultado;
+        RequisicaoAssincrona requisicao = new RequisicaoAssincrona();
+        try {
+            JSONObject resultado_requisicao = requisicao.execute(RequisicaoAssincrona.INSCREVER_ALUNO_EM_MATERIA, "abc@gmail.com", "MDS01").get();
+            if (resultado_requisicao.getString("status").equals("ok")){resultado = 1.0;}
+            else{
+                if(resultado_requisicao.getString("descricao").equals("Não é possível cadastrá-lo nesta matéria, pois ela está inativa")){resultado = 2.0;}
+                else if(resultado_requisicao.getString("descricao").equals("Erro na requisição. Aluno ou matéria não encontrados.")){resultado = 3.0;}
+                else {resultado = 0;}
+            }
+
+            assertEquals(3.0, resultado, 0.0f);
+        }catch (InterruptedException | ExecutionException| JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void buscarProfessor1(){
+        double resultado;
+        RequisicaoAssincrona requisicao = new RequisicaoAssincrona();
+        try {
+            JSONObject resultado_requisicao = requisicao.execute(RequisicaoAssincrona.BUSCAR_PERFIL_DO_USUARIO, "rophos.rb@gmail.com").get();
+            if(resultado_requisicao.getString("status").equals("ok")){resultado = 1.0;}
+            else{
+                if(resultado_requisicao.getString("descricao").equals("Perfil não encontrado")){resultado = 2.0;}
+                else{resultado = 0;}
+            }
+
+            assertEquals(1.0,resultado,0.0f);
+        }catch (InterruptedException | ExecutionException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void buscarProfessor2(){
+        double resultado;
+        RequisicaoAssincrona requisicao = new RequisicaoAssincrona();
+        try {
+            JSONObject resultado_requisicao = requisicao.execute(RequisicaoAssincrona.BUSCAR_PERFIL_DO_USUARIO, "abc@gmail.com").get();
+            if(resultado_requisicao.getString("status").equals("ok")){resultado = 1.0;}
+            else{
+                if(resultado_requisicao.getString("descricao").equals("Perfil não encontrado")){resultado = 2.0;}
+                else{resultado = 0;}
+            }
+
+            assertEquals(2.0,resultado,0.0f);
+        }catch (InterruptedException | ExecutionException | JSONException e) {
             e.printStackTrace();
         }
     }
